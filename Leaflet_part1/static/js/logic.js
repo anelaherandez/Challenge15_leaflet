653ee8fdb815f25d7589ee5d9cf224c982b4a5de
anelaherandez <andreat818@gmail.com>
2024-03-09 12:23:37 -0700

    updates

---
+++ Leaflet_part1/static/js/logic.js
@@ -0,0 +1,120 @@
+// Store our API endpoint as queryUrl.
+let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
+
+// Perform a GET request to the query URL/
+d3.json(queryUrl).then(function (data) {
+  // Once we get a response, send the data.features object to the createFeatures function.
+  createFeatures(data.features);
+});
+
+//function to create magnitude
+function markerSize(magnitude) {
+  return magnitude*2000;
+}
+
+// Function to determine marker color by depth
+function chooseColor(depth){
+  if (depth < 10) return "#00FF00";
+  else if (depth < 30) return "greenyellow";
+  else if (depth < 50) return "yellow";
+  else if (depth < 70) return "orange";
+  else if (depth < 90) return "orangered";
+  else return "#FF0000";
+}
+
+
+function createFeatures(earthquakeData) {
+
+  // Define a function that we want to run once for each feature in the features array.
+  // Give each feature a popup that describes the place and time of the earthquake.
+  function onEachFeature(feature, layer) {
+    layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
+  }
+
+  // Create a GeoJSON layer that contains the features array on the earthquakeData object.
+  // Run the onEachFeature function once for each piece of data in the array.
+  let earthquakes = L.geoJSON(earthquakeData, {
+    onEachFeature: onEachFeature,
+
+  // Point to layer used to alter markers
+  pointToLayer: function(feature, latlng) {
+
+    // Determine the style of markers based on properties
+    var markers = {
+      radius: markerSize(feature.properties.mag),
+      fillColor: chooseColor(feature.geometry.coordinates[2]),
+      fillOpacity: 0.5,
+      color: "black",
+      stroke: true,
+      weight: 0.5
+    }
+    return L.circle(latlng,markers);
+  }
+});
+  // Send our earthquakes layer to the createMap function/
+  createMap(earthquakes);
+}
+
+function createMap(earthquakes) {
+
+  // Create the base layers.
+  let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  })
+
+  let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+  });
+
+  // Create a baseMaps object.
+  let baseMaps = {
+    "Street Map": street,
+    "Topographic Map": topo
+  };
+
+  // Create an overlay object to hold our overlay.
+  let overlayMaps = {
+    Earthquakes: earthquakes
+  };
+
+  // Create map, giving it the streetmap, topo, and earthquakes layers to display on load.
+  let myMap = L.map("map", {
+    center: [
+      37.09, -95.71
+    ],
+    zoom: 5,
+    layers: [street, topo, earthquakes]
+  });
+
+  // Create a layer control.
+  // Pass it our baseMaps and overlayMaps.
+  // Add the layer control to the map.
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
+
+  //create legend
+  var legend = L.control({position: "bottomright"});
+  legend.onAdd = function() {
+    var div = L.DomUtil.create('div', 'info legend');
+    var depth = [-10, 10, 30, 50, 70, 90];
+    var labels = [];
+    var legendInfo = "<h4>Magnitude</h4>";
+
+    div.innerHTML = legendInfo
+
+    // go through each magnitude item to label and color the legend
+    // push to labels array as list item
+    for (var i = 0; i < depth.length; i++) {
+          labels.push('<ul style="background-color:' + chooseColor(depth[i] + 1) + '"> <span>' + depth[i] + (depth[i + 1] ? '&ndash;' + depth[i + 1] + '' : '+') + '</span></ul>');
+        }
+
+      // add each label list item to the div under the <ul> tag
+      div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+    
+    return div;
+  };
+  //
+  legend.addTo(myMap)
+};
+
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Challenge15_leaflet
+The USGS is interested in building a new set of tools that will allow them to visualize their earthquake data. They collect a massive amount of data from all over the world each day, but they lack a meaningful way of displaying it. In this challenge, you have been tasked with developing a way to visualize USGS data that will allow them to better educate the public and other government organizations (and hopefully secure more funding) on issues facing our planet.
+
+## Create Earthquake Visualization
+1. Get your dataset. To do so, follow these steps:
+    The USGS provides earthquake data in a number of different formats, updated every 5 minutes. Visit the USGS GeoJSON FeedLinks to an external site. page and choose a dataset to visualize. 
+2. When you click a dataset, you will be given a JSON representation of that data. Use the URL of this JSON to   
+    pull in the data for the visualization. 
+3. Import and visualize the data by doing the following:
+    Using Leaflet, create a map that plots all the earthquakes from your dataset based on their longitude and latitude. Your data markers should reflect the magnitude of the earthquake by their size and the depth of the earthquake by color. Earthquakes with higher magnitudes should appear larger, and earthquakes with greater depth should appear darker in color.
+    Include popups that provide additional information about the earthquake when its associated marker is clicked. Create a legend that will provide context for your map data.
+
+## References
+For this project I chose the 30 day GeoJSON link from the USGS website. I used Bootcamp activites as a guideline as well as StackOverflow. 
